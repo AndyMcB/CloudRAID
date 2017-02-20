765ef96109be5f633f56bcc527291bb50c076e3e
AMCBR <a.mcbreen1@outlook.com>
2017-02-20 12:52:21 +0000

    added san=mples and creds

--- raid/RAID5.py
+++ raid/RAID5.py
@@ -5,3 +5,14 @@ class RAID5:
 
     def __init__(self):
         pass
+
+
+    def strip(self):
+        pass
+
+    def reconstruct(self):
+        pass
+
+    def get_binary(self):
+        pass
+
